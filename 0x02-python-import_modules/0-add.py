7ac15167864cab7036c80893039fc12ced902c50
mahdixabid <mahdixabid>
2021-04-29 18:43:31 +0100

    fix code v2

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import add_0 as operation
+    from add_0 import add
     a = 1
     b = 2
-    addition = operation.add(a , b)
-    print(str(a) + " + " + str(b) + " = " ,  addition)
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
