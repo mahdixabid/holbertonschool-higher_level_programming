7ff0716ff5a099155ccb4b9077216cf419fed1a3
mahdixabid <mahdixabid>
2021-04-29 18:37:18 +0100

    fix v1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import add_0 as operation
-a=1
-b=2
-addition = operation.add(a,b)
-print(str(a) + " + " + str(b) + " = ",  addition)
+if __name__ == '__main__':
+    import add_0 as operation
+    a = 1
+    b = 2
+    addition = operation.add(a , b)
+    print(str(a) + " + " + str(b) + " = " ,  addition)
