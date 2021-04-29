d465cf48ff63e72a6beecd7bff28762fcf34104e
mahdixabid <mahdixabid>
2021-04-29 21:08:15 +0100

    fix v3

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
+    from calculator_1 import addition, subtraction, multiplication, division
     a = 10
     b = 5
-    from calculator_1 import addition, subtraction, multiplication, division
     print("{:d} + {:d} = {:d}".format(a, b, addition(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, subtraction(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, multiplication(a, b)))
