66e97782bd65f582deef93c32189b4e8985ae389
mahdixabid <mahdixabid>
2021-05-18 18:52:09 +0100

    fix

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module Square"""
+
+
+class Square:
+    """Add to the class an private instance attribute
+    """
+    def __init__(self, __size):
+        self.__size = __size
