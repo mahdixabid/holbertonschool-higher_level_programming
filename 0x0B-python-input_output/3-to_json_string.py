cef5cd0996839b6f052948704590b32f71f852ed
mahdixabid <mahdiabid158@gmail.com>
2021-06-02 15:03:08 +0100

    Returns the JSON representation of an object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string)
+    """
+    return (json.dumps(my_obj))
