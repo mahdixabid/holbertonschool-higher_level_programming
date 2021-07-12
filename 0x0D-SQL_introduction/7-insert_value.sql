a6e92bc761363a6416bd997a717aad4937516c23
black_boot.exe <77060124+mahdixabid@users.noreply.github.com>
2021-07-12 23:15:54 +0100

    fix

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- Inserts a new row in the table
-INSERT INTO first_table(id, name) VAlUES(89, 'Coding School');
+--  inserts a new row in the table first_table
+INSERT INTO first_table (id,name) value (89,'Holberton School');
