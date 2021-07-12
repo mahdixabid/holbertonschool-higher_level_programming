a96b94b7e6a983654fbf84fcdfc177b605a98c49
black_boot.exe <77060124+mahdixabid@users.noreply.github.com>
2021-07-12 22:04:13 +0100

    single commit

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- Update a field in the second_table
+UPDATE `second_table` SET `score` = 10 WHERE `name` = 'Bob';
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,2 @@
+-- Removes all records of a specific condition
+DELETE FROM `second_table` WHERE score <= 5;
