1319d239e65e87fcf54614d63fb63eddbd667e31
92546518Mahdi@@ <mahdiabid158@gmail.com>
2021-08-08 17:48:53 +0100

    fix

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-# list all state objects from the database
+""" lists all State objects from the database hbtn_0e_6_usa"""
 
-if __name__ == "__main__":
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sys import argv
-    from model_state import Base, State
-    from sqlalchemy.engine.url import URL
 
-    db = {'drivername': 'mysql+mysqldb',
-          'host': 'localhost',
-          'port': '3306',
-          'username': argv[1],
-          'password': argv[2],
-          'database': argv[3]}
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+import model_state
+from model_state import Base, State
+import sys
 
-    url = URL(**db)
-    engine = create_engine(url, pool_pre_ping=True)
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-
-    for state in session.query(State).order_by(State.id).all():
+    for state in session.query(model_state.State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
-
     session.close()
