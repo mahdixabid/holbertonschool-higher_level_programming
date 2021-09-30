abd76d17e42fd2e1db3ddaaeb17f3fc6c5d3af6f
mahdixabid <mahdiabid158@gmail.com>
2021-09-30 14:06:04 +0200

    fix style

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let fs = require('fs');
+const fs = require('fs');
 
-let data = process.argv[3];
+const data = process.argv[3];
 
 fs.writeFile(process.argv[2], data, function (err, data) {
   if (err) console.log(err);
